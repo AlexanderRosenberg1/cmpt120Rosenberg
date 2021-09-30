fba275ae0fb044fadd1dc364eb96431373ed2ac6
AlexanderRosenberg1 <89876098+AlexanderRosenberg1@users.noreply.github.com>
2021-09-30 14:19:27 -0400

    Add files via upload

---
+++ old_MacDonald.py
@@ -0,0 +1,20 @@
+#old_MacDonald.py
+
+def mcd(animal,sound):
+    print("Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!")
+    print("And on the farm he had a",animal + ", Ee-igh, Ee-igh, Oh!")
+    print(sound)
+    print("Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!")
+    
+def main():
+    mcd("cow","With a moo, moo here and a moo, moo there.\nHere a moo, there a moo, everywhere a moo, moo." )
+    print("======================================================================")
+    mcd("pig","With a oink, oink here and a oink, oink there.\nHere a oink, there a oink, everywhere a oink, oink." )
+    print("======================================================================")
+    mcd("hen","With a cluck, cluck here and a cluck, cluck there.\nHere a cluck, there a cluck, everywhere a cluck, cluck." )
+    print("======================================================================")
+    mcd("dog","With a woof, woof here and a woof, woof there.\nHere a woof, there a woof, everywhere a woof, woof." )
+    print("======================================================================")
+    mcd("sheep","With a bah, bah here and a bah, bah there.\nHere a bah, there a bah, everywhere a bah, bah." )
+main()
+
